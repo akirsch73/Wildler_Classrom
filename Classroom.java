162204a999c77bde7511b23d8f559083cda89aa0
Alexander Kirsch <a.kirsch@gmx.de>
2019-11-22 10:30:06 +0100

    Inital Commit

---
+++ Classroom.java
@@ -0,0 +1,15 @@
+public class Classroom
+{
+	
+	public static void main (String[] args)
+	{
+	Wilder1 varperson = new Wilder1 ("Alexander", true);	
+	Wilder1 fifi = new Wilder1 ("Peter", false);		
+	
+	System.out.println (varperson.WhoIAm());
+	System.out.println (fifi.WhoIAm());
+	}
+
+}
+
+
